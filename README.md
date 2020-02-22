d6f2594356dbb41ab445e8275b83bc0b11afce0e
Graddlly <34636667+Graddlly@users.noreply.github.com>
2020-02-22 22:35:33 +0500

    [POINT] Test point №1

--- README.md
+++
@@ -1,8 +0,0 @@
-# Xeolis Bot [Discord] : Created by Graddlly
-
-![Build Status](https://travis-ci.com/Graddlly/XeolisBot.svg?branch=master) ![Node.js CI](https://github.com/Graddlly/XeolisBot/workflows/Node.js%20CI/badge.svg?branch=master)
-
-* [Discord channel](https://discord.gg/C8UakNd)
-* [See license](LICENSE)
-* [See code of conduct](CODE_OF_CONDUCT.md)
-* [Site of XeolisProject](https://xeolisproject.ru/)
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ if (process.version.slice(1).split(".")[0] < 8)
     throw new Error("Node 8.0.0 or higher is required.");
 
 try {
-    var { Client, Collection } = require("discord.js");
-    var { config } = require("dotenv");
     var { randomInteger, formatDate } = require("./functions.js");
-    var fs = require("fs");
-    var mes = require("./mes_events.js");
+    var { CommandoClient } = require("discord.js-commando");
+    var { Structures } = require("discord.js");
+    var conf = require("./config.json");
+    var path = require("path");
 } catch (e) {
     console.log(e.stack);
     console.log(process.version);
@@ -14,23 +14,45 @@ try {
     process.exit();
 }
 
-const client = new Client({
-    disableEveryone: true
+const client = new CommandoClient({
+    unknownCommandResponse: false,
+    commandPrefix: conf.prefix,
+    disableEveryone: true,
+    owner: conf.ownerid
 });
 
-const prefix = ">";
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ["other", "random types of command group"],
+        ["moderation", "Moderation Command Group"],
+        ["games", "Games Command Group"],
+        ["music", "Music Command Group"],
+        ["gifs", "Gif Command Group"]
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, "commands"));
 
-client.commands = new Collection();
-client.aliases = new Collection();
-
-client.categories = fs.readdirSync("./commands/");
-
-config({
-    path: __dirname + "/.env"
-});
-
-["command"].forEach(handler => {
-    require(`./handler/${handler}`)(client);
+Structures.extend("Guild", Guild => {
+    class MusicGuild extends Guild {
+        constructor(client, data) {
+            super(client, data);
+            this.musicData = {
+                queue: [],
+                isPlaying: false,
+                nowPlaying: null,
+                songDispatcher: null
+            };
+            this.triviaData = {
+                isTriviaRunning: false,
+                wasTriviaEndCalled: false,
+                triviaQueue: [],
+                triviaScore: new Map()
+            };
+        }
+    }
+    return MusicGuild;
 });
 
 client.on("ready", () => {
@@ -46,36 +68,13 @@ client.on("ready", () => {
     ];
 
     setInterval(function() {
-        client.user.setPresence({
-            status: "online",
-            game: {
-                name: statuses[randomInteger(0, 3)],
+        client.user.setActivity(
+            statuses[randomInteger(0, parseInt(statuses.length))], {
                 type: "STREAMING",
                 url: "https://www.twitch.tv/graddllyma"
             }
-        });
+        );
     }, 10000);
-});
-
-client.on("message", async message => {
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-    if (!message.member)
-        message.member = await message.guild.fetchMember(message);
-
-    const args = message.content
-        .slice(prefix.length)
-        .trim()
-        .split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-
-    if (cmd.length === 0) return;
-
-    let command = client.commands.get(cmd);
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-    if (command) command.run(client, message, args);
 });
 
 //Event Handlers (Basic System)
@@ -88,198 +87,9 @@ client.on("reconnecting", () =>
     console.log("Я переподключаюсь... Подождите меня!")
 );
 
-//Event Handlers (Log System)
-client.on("guildMemberAdd", async member => {
-    var channel = member.guild.channels.find(
-        x => x.name === `${process.env.LOGS}`
-    );
-    if (!channel) return;
-    channel.send(
-        mes.memberJoined(
-            member.user,
-            member.user.id,
-            member.user.displayAvatarURL,
-            member.guild.id,
-            formatDate(member.user.createdAt)
-        )
-    );
-});
-
-client.on("guildBanAdd", async(guild, user) => {
-    var channel = guild.channels.find(x => x.name === `${process.env.LOGS}`);
-    if (!channel) return;
-    guild
-        .fetchBan(user)
-        .then(({ reason }) =>
-            channel.send(
-                mes.memberBanned(user, user.id, user.displayAvatarURL, reason)
-            )
-        );
-});
-
-client.on("guildMemberRemove", async member => {
-    var channel = member.guild.channels.find(
-        x => x.name === `${process.env.LOGS}`
-    );
-    if (!channel) return;
-    channel.send(
-        mes.memberLeft(
-            member.user,
-            member.user.id,
-            member.user.displayAvatarURL,
-            member.guild.id,
-            formatDate(member.joinedAt),
-            formatDate(member.user.createdAt)
-        )
-    );
-});
-
-client.on("guildBanRemove", async(guild, user) => {
-    var channel = guild.channels.find(x => x.name === `${process.env.LOGS}`);
-    if (!channel) return;
-    channel.send(mes.memberUnbanned(user, user.id, user.displayAvatarURL));
-});
-
-client.on("messageUpdate", async(oldMessage, newMessage) => {
-    var channel = oldMessage.guild.channels.find(
-        x => x.name === `${process.env.LOGS}`
-    );
-    if (!channel) return;
-    if (oldMessage.content === newMessage.content) return;
-    if (newMessage.author.bot) return;
-    if (channel.type === "undefined") return;
-    channel.send(
-        mes.messageEdited(
-            oldMessage,
-            newMessage,
-            newMessage.author.displayAvatarURL
-        )
-    );
-});
-
-client.on("messageDelete", async message => {
-    var channel = message.guild.channels.find(
-        x => x.name === `${process.env.LOGS}`
-    );
-    if (!channel) return;
-    if (message.author.bot) return;
-    if (channel.type === "undefined") return;
-    channel.send(
-        mes.messageDeleted(message, message.author, message.author.displayAvatarURL)
-    );
-});
-
-client.on("messageDeleteBulk", async messages => {
-    var channel = messages
-        .first()
-        .guild.channels.find(x => x.name === `${process.env.LOGS}`);
-    if (!channel) return;
-    channel.send(
-        mes.bulkDelete(
-            messages.first().guild.iconURL,
-            messages.first().guild.id,
-            messages.first().channel
-        )
-    );
-});
-
-client.on("channelCreate", async channel => {
-    var addchan = channel.client.guilds.find(ch =>
-        ch.channels.find(x => x.id === `${channel.id}`)
-    );
-    var channellog = channel.guild.channels.find(
-        ch => ch.name === `${process.env.LOGS}`
-    );
-    if (!channellog) return;
-    if (channel.type === "undefined") return;
-    channellog.send(
-        mes.channelCreated(
-            addchan.iconURL,
-            channel.id,
-            channel.type,
-            channel.name,
-            formatDate(channel.createdAt)
-        )
-    );
-});
-
-client.on("channelDelete", async channel => {
-    var channellog = channel.guild.channels.find(
-        ch => ch.name === `${process.env.LOGS}`
-    );
-    var addchan = channellog.client.guilds.find(ch =>
-        ch.channels.find(x => x.id === `${channellog.id}`)
-    );
-    if (!channellog) return;
-    if (channel.type === "undefined") return;
-    channellog.send(
-        mes.channelDeleted(
-            addchan.iconURL,
-            channel.id,
-            channel.type,
-            channel.name,
-            formatDate(channel.createdAt)
-        )
-    );
-});
-
-client.on("roleCreate", async role => {
-    var addchan = role.client.guilds.find(ch =>
-        ch.roles.find(x => x.id === `${role.id}`)
-    );
-    var channellog = role.guild.channels.find(
-        ch => ch.name === `${process.env.LOGS}`
-    );
-    if (!channellog) return;
-    channellog.send(
-        mes.roleCreated(
-            addchan.iconURL,
-            role.id,
-            role.name,
-            role.permissions,
-            formatDate(role.createdAt)
-        )
-    );
-});
-
-client.on("roleDelete", async role => {
-    var addchan = role.client.guilds.find(ch =>
-        ch.roles.find(x => x.id === `${role.id}`)
-    );
-    var channellog = role.guild.channels.find(
-        ch => ch.name === `${process.env.LOGS}`
-    );
-    if (!channellog) return;
-    channellog.send(
-        mes.roleDeleted(
-            addchan.iconURL,
-            role.id,
-            role.name,
-            role.permissions,
-            formatDate(role.createdAt)
-        )
-    );
-});
-
-client.on("guildMemberUpdate", async(oldMember, newMember) => {
-    var channellog = newMember.guild.channels.find(
-        ch => ch.name === `{${process.env.LOGS}}`
-    );
-    if (!channellog) return;
-    if (oldMember.displayName !== newMember.displayName)
-        channellog.send(
-            mes.nickChanged(
-                oldMember.user.displayAvatarURL,
-                oldMember.user.tag,
-                newMember.user.tag,
-                newMember.user.id
-            )
-        );
-});
-
 process.on("unhandledRejection", reason => {
     console.error(reason);
     process.exit(1);
 });
 
-client.login(process.env.TOKEN).catch(e => console.log(e));
+client.login(conf.token).catch(e => console.log(e));
